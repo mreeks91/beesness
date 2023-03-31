571f9ab31ee721ecf18c30aa070c846b44ddf6e1
mreeks91 <mreeks91@gmail.com>
2023-03-31 07:35:18 -0400

    Gathering data and training first model.

---
+++ data_collection.py
@@ -0,0 +1,38 @@
+import urllib.request
+from PIL import Image
+from settings import TSV_DIRECTORY, IMAGE_DIRECTORY
+import os
+import pandas as pd
+
+def image_urls_from_file(relpath,file_type = 'txt', sample_proportion = .05):
+    if file_type == 'tsv' or file_type == 'txt':
+        df = pd.read_table(TSV_DIRECTORY+'/'+relpath+'.'+file_type)
+    elif file_type == 'csv':
+        df = pd.read_csv(TSV_DIRECTORY+'/'+relpath+'.'+file_type)
+    df.dropna(subset = ['identifier'],inplace = True)
+    data_sample = df.sample(round(df.shape[0]*sample_proportion))
+    return data_sample[['gbifID','identifier']]
+
+def download_images(url_df,class_name,image_size=(400,400)):
+    path = IMAGE_DIRECTORY + '/' + class_name
+    if not os.path.exists(path):
+        os.makedirs(path)
+    for url in url_df.iterrows():
+        filename = class_name+str(url[1][0])+'.jpg'
+        fullpath = path+'/'+filename
+        try:
+            urllib.request.urlretrieve(str(url[1][1]), filename=fullpath)
+        except (urllib.error.HTTPError,urllib.error.URLError,TypeError):
+            pass
+        try:
+            im = Image.open(fullpath)
+            im = im.thumbnail(image_size)
+            im = im.save(fullpath)
+        except (AttributeError,FileNotFoundError):
+            pass
+
+if __name__ == "__main__":
+    classes = ['vespa']
+    for cls in classes:
+        sample = image_urls_from_file(cls)
+        download_images(sample,cls)
---
+++ train_test_split.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+from torch.utils.data import random_split
+from torch import Generator
+
+PATH = 'E:/Bees/Images'
+TEST_RATIO = 0.2 
+
+gen = Generator().manual_seed(314)
+
+
+
+
+
